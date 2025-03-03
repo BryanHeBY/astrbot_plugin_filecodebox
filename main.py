edba5ee449498c207c7203d488d08f00e16d915d
BryanHeBY <bryanheby@qq.com>
2025-03-03 18:13:40 +0800

    add file

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.api.message_components import File
 import requests
+import tempfile
 
 @register("filecodebox", "BryanHe", "FileCodeBox配套插件", "1.0.0", "https://github.com/BryanHeBY/astrbot_plugin_filecodebox")
 class FileCodeBox(Star):
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
         self.url = config.get("filecodebox_url")
+        if self.url[-1] == "/":
+            self.url = self.url[:-1]
     
     @filter.command("send")
-    async def send(self, event: AstrMessageEvent):
+    async def send(self, event: AstrMessageEvent, text: str|None = None):
         '''这是发送文件的指令''' 
-        message_str = event.message_str # 用户发的纯文本消息字符串
+        if text is None:
+            yield event.plain_result("请输入要发送的内容")
+            return
         form_data = {
-            "text": message_str,
+            "text": text,
             "expire_value": 1,
             "expire_style": "day"
         }
@@ -40,5 +46,18 @@ class FileCodeBox(Star):
             detail = r_json["detail"]
             if detail["name"] == "Text":
                 yield event.plain_result(f"{detail['text']}")
+            else:
+                download_url = f"{self.url}{detail['text']}"
+                r = requests.get(download_url)
+                tmp = tempfile.NamedTemporaryFile(delete=True)
+                try:
+                    tmp.write(r.content)
+                    tmp.flush()
+                    chain = [
+                        File(name=detail['name'], file=tmp.name)
+                    ]
+                    yield event.chain_result(chain)
+                finally:
+                    tmp.close()  # 文件关闭即删除
         else :
             yield event.plain_result(f"取件失败，错误码: {r_json['code']}")
