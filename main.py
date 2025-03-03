fdd40c9e27832db03732dc03ae704bbabd94e8b7
BryanHeBY <bryanheby@qq.com>
2025-03-03 17:21:18 +0800

    init str

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import requests
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("filecodebox", "BryanHe", "FileCodeBox配套插件", "1.0.0", "https://github.com/BryanHeBY/astrbot_plugin_filecodebox")
+class FileCodeBox(Star):
+    def __init__(self, context: Context, config: dict):
         super().__init__(context)
+        self.url = config.get("filecodebox_url")
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("send")
     async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
+        '''这是发送文件的指令''' 
         message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        form_data = {
+            "text": message_str,
+            "expire_value": 1,
+            "expire_style": "day"
+        }
+        r = requests.post(
+            url=f"{self.url}/share/text/",
+            data=form_data)
+        r_json = r.json()
+        if r["code"] == 200:
+            code = r_json["detail"]["code"]
+            yield event.plain_result(f"发送成功，取件码: {code}")
+        else :
+            yield event.plain_result(f"发送失败，错误码: {r['code']}")
+        
+    @filter.command("receive")
+    async def helloworld(self, event: AstrMessageEvent, code: str):
+        '''这是收取文件的指令''' 
+        json_data = { "code": code }
+        r = requests.post(
+            url=f"{self.url}/share/select/",
+            json=json_data)
+        r_json = r.json()
+        if r["code"] == 200:
+            detail = r_json["detail"]
+            if detail["name"] == "Text":
+                yield event.plain_result(f"{detail["text"]}")
+        else :
+            yield event.plain_result(f"取件失败，错误码: {r['code']}")
